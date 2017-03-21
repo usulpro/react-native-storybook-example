718c1b23c758512e1d2590e5d1e825ad4b8e8323
Gytis Vinclovas <gytis.vinclovas@gmail.com>
2017-03-21 12:31:30 +0200

    rewritten component parser.

--- storybook/addons/storybook-addon-usage/preview.js
+++ storybook/addons/storybook-addon-usage/preview.js
@@ -1,95 +1,64 @@
 import React from 'react';
-import addons from "@kadira/storybook-addons";
-import {EVENT_ID} from "./";
-import _ from "lodash";
-
-const getSpaces = depth => _.repeat('\u00a0', depth * 4);
-
-function getStringRepresentation(story, depth=1) {
-
-  const name = story.type.name || story.type.displayName;
-
-
-  const props = _.toPairs(story.props).filter(prop => {
-    return !story.type.defaultProps || story.type.defaultProps[prop[0]] !== prop[1];
-  });
-
-  if (props.length === 0) {
-    return `${getSpaces(depth-1)}<${name}/>\n`;
-  }
-
-  let first = `${getSpaces(depth-1)}<${name}`;
-
-  if (props.length === 1 && props[0][0] === 'children') {
-    first += '>\n';
-  } else {
-    first += `\n`;
-  }
-
-  props.forEach((p) => {
-    let value;
-
-    if (p[0] === 'children') {
-      //do nothing
-    } else {
-      if (typeof p[1] === 'function') {
-        value = p[1].name ? `{${p[1].name}}` : '{anonymous}';
-      } else if (typeof p[1] === 'string') {
-        value = `${JSON.stringify(p[1])}`;
-      }
-      else if (_.isObject(p[1]) && !_.isArray(p[1])) {
-        value = `{${objToString(p[1])}}`;
-      }
-      else {
-        value = `{${JSON.stringify(p[1])}}`;
-      }
-
-      first += `${_.repeat('\u00a0', depth * 4)}${p[0]}=${value}\n`;
-    }
-  });
-
-  if (story.props.children) {
-    if (story.props.length > 1) {
-      first += `${getSpaces(depth-1)}>\n`;
-    }
-
-    React.Children.map(story.props.children, story => {
-      console.log(story);
-      first += _.isObject(story)
-        ? getStringRepresentation(story, depth + 1)
-        : getSpaces(depth) + story + '\n';
-    });
-
-    first += `${getSpaces(depth-1)}</${name}>\n`;
-  } else {
-    first += getSpaces(depth-1) + "/>\n";
-  }
-
-  return first;
-}
+import addons from '@kadira/storybook-addons';
+import {EVENT_ID} from './';
+import _ from 'lodash/fp';
 
 export function Usage(fn) {
   let story = fn();
-  const first = getStringRepresentation(story, 1);
+  const storybook = getComponentRepresentation(story);
+
   const channel = addons.getChannel();
-  channel.emit(EVENT_ID, {storybook: first});
+  channel.emit(EVENT_ID, {storybook});
   return fn();
 }
 
-function objToString(obj) {
+const getSpaces = depth => _.repeat(depth * 4, '\u00a0');
 
-  var str = '{';
-  for(let p in obj){
-    if (obj.hasOwnProperty(p)) {
-      let value;
-      if(_.isObject(obj[p]) && !_.isArray(obj[p])) {
-        value = objToString(obj[p]);
-      }else{
-        value = JSON.stringify(obj[p]);
-      }
-      str += p + ': ' + value;
-    }
+const getArrayOfAllNonDefaultProps = (props, defaultProps) => _.toPairs(props).filter(
+  ([name, value]) => !defaultProps || defaultProps[name] !== value
+);
+
+const getFuncName = value => value.name || 'anonymous';
+const isObject = (value) => _.isObject(value) && !_.isArray(value);
+
+const renderProp = _.cond([
+  [_.isFunction, getFuncName],
+  [isObject, objToString],
+  [_.stubTrue, JSON.stringify]
+]);
+
+function objToString(obj) {
+  const representation =
+    _.toPairs(obj)
+      .map(([key, value]) => `${key}: ${isObject(value) ? objToString(value) : JSON.stringify(value)}`)
+      .join(', ');
+
+  return `{${representation}}`;
+}
+
+function getComponentRepresentation(story) {
+  const name = story.type.name || story.type.displayName;
+  const props = getArrayOfAllNonDefaultProps(story.props, story.type.defaultProps);
+
+  if (props.length === 0) {
+    return `<${name}/>\n`;
   }
-  str += '}';
-  return str;
+
+  const propsWithoutChildren = props.filter(([name, value]) => name !== 'children');
+  const propsRepresentation = propsWithoutChildren.map(
+    ([name, value]) => getSpaces(1) + `${name}={${renderProp(value)}}\n`
+  ).join('');
+
+  const childRepresentation = story.props.children && React.Children.map(story.props.children,
+      child => _.isObject(child)
+        ? getComponentRepresentation(child).split('\n').map(x => x && getSpaces(1) + x).join('\n')
+        : getSpaces(1) + child + '\n'
+    ).join('');
+
+  return [
+    `<${name}`,
+    propsRepresentation && '\n' + propsRepresentation,
+    !childRepresentation && '/>\n',
+    childRepresentation && '>\n' + childRepresentation + `</${name}>\n`
+  ].filter(_.identity).join('');
 }
